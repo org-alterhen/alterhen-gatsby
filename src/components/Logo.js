a281b175d6a153be89b02adf3caee759e1fee20e
kylegrover <kyle@kylegrover.com>
2021-08-29 16:47:29 -0700

    fix class className

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const Logo = ({ className }) => (
-  <span className={`logo-text ${className}`}>alter<span class="logo-text-HEN">HEN</span></span>
+  <span className={`logo-text ${className}`}>alter<span className="logo-text-HEN">HEN</span></span>
 )
 
 Logo.propTypes = {
