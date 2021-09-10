1663f1aead162d2dbb6657481a9dc7682de89ab6
kylegrover <kyle@kylegrover.com>
2021-09-10 15:50:32 -0700

    artist cms preview

--- src/cms/preview-templates/ArtistPostPreview.js
+++ src/cms/preview-templates/ArtistPostPreview.js
@@ -5,8 +5,19 @@ import { ArtistPostTemplate } from '../../templates/artist-post'
 const ArtistPostPreview = ({ entry, widgetFor }) => {
   return (
     <ArtistPostTemplate
-      description={entry.getIn(['data', 'description'])}
+      bio={entry.getIn(['data', 'bio'])}
+      statement={entry.getIn(['data', 'statement'])}
+      country={entry.getIn(['data', 'country'])}
+      website={entry.getIn(['data', 'website'])}
+      name={entry.getIn(['data', 'name'])}
+      instagram={entry.getIn(['data', 'instagram'])}
+      twitter={entry.getIn(['data', 'twitter'])}
+      facebook={entry.getIn(['data', 'facebook'])}
+      linktree={entry.getIn(['data', 'linktree'])}
+      henlink={entry.getIn(['data', 'henlink'])}
+      tumblr={entry.getIn(['data', 'tumblr'])}
       title={entry.getIn(['data', 'title'])}
+      midbanner={entry.getIn(['data', 'midbanner'])}
     />
   )
 }
