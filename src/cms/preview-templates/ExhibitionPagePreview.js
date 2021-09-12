3d8ca65ace34efdc4dc45c66b05b4dbd81d1e074
kylegrover <kyle@kylegrover.com>
2021-09-11 18:06:55 -0700

    fix exhibition preview

--- src/cms/preview-templates/ExhibitionPagePreview.js
+++ src/cms/preview-templates/ExhibitionPagePreview.js
@@ -3,45 +3,15 @@ import PropTypes from 'prop-types'
 import { ExhibitionPageTemplate } from '../../templates/exhibition-page'
 
 const ExhibitionPagePreview = ({ entry, getAsset }) => {
-  const entryBlurbs = entry.getIn(['data', 'intro', 'blurbs'])
-  const blurbs = entryBlurbs ? entryBlurbs.toJS() : []
-
-  const entryTestimonials = entry.getIn(['data', 'testimonials'])
-  const testimonials = entryTestimonials ? entryTestimonials.toJS() : []
-
-  const entryPricingPlans = entry.getIn(['data', 'pricing', 'plans'])
-  const pricingPlans = entryPricingPlans ? entryPricingPlans.toJS() : []
+  const entryObjkts = entry.getIn(['data', 'objkts'])
+  const objkts = entryObjkts ? entryObjkts.toJS() : []
 
   return (
     <ExhibitionPageTemplate
-      image={getAsset(entry.getIn(['data', 'image']))}
+      artist={entry.getIn(['data', 'artist'])}
       title={entry.getIn(['data', 'title'])}
-      heading={entry.getIn(['data', 'heading'])}
       description={entry.getIn(['data', 'description'])}
-      intro={{ blurbs }}
-      main={{
-        heading: entry.getIn(['data', 'main', 'heading']),
-        description: entry.getIn(['data', 'main', 'description']),
-        image1: {
-          image: getAsset(entry.getIn(['data', 'main', 'image1', 'image'])),
-          alt: entry.getIn(['data', 'main', 'image1', 'alt']),
-        },
-        image2: {
-          image: getAsset(entry.getIn(['data', 'main', 'image2', 'image'])),
-          alt: entry.getIn(['data', 'main', 'image2', 'alt']),
-        },
-        image3: {
-          image: getAsset(entry.getIn(['data', 'main', 'image3', 'image'])),
-          alt: entry.getIn(['data', 'main', 'image3', 'alt']),
-        },
-      }}
-      fullImage={entry.getIn(['data', 'full_image'])}
-      testimonials={testimonials}
-      pricing={{
-        heading: entry.getIn(['data', 'pricing', 'heading']),
-        description: entry.getIn(['data', 'pricing', 'description']),
-        plans: pricingPlans,
-      }}
+      objkts={{ objkts }}
     />
   )
 }
