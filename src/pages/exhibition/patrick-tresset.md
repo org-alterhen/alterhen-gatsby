e4ace4042349c7c908cf431644bd0b1576ee4756
kylegrover <kyle@kylegrover.com>
2021-09-11 17:45:22 -0700

    exhibition pages

--- src/pages/exhibition/patrick-tresset.md
+++ src/pages/exhibition/patrick-tresset.md
@@ -1,8 +1,22 @@
 ---
 templateKey: exhibition-page
 artist: Patrick Tresset
-title: Exhibition Title
-image: /img/artist-thumbs/patrick-tresset.png
+title: Patrick Tresset Test
 description: "Demo Gallery"
-objkts: [12345,23456,34567]
+objkts: 
+    - objkt: 12345
+      title: test 12345
+      desc: test description 12345 abcdef
+      image: http://www.fillmurray.com/800/600
+    - objkt: 23456
+      title: test 23456
+      desc: >-
+        much longer test description for objkt number 23456 
+        including line breaks, maybe those will work
+        maybe they won't let's find out
+      image: http://www.fillmurray.com/1800/2400
+    - objkt: 34567
+      title: test 34567
+      desc: test description 34567 la di da di
+      image: http://www.fillmurray.com/1000/1000
 ---
--- src/templates/exhibition-page.js
+++ src/templates/exhibition-page.js
@@ -6,23 +6,6 @@ import BasicHeader from '../components/BasicHeader'
 
 import Exhibition from '../components/Exhibition'
 
-const test_objkts = [{
-  title: 'Demo 1',
-  description: 'Filler Image\n\nDemo 1 is a demo artwork to test the exhibitions\n\nIt will be replaced with the artists works',
-  url: 'https://hicetnunc.xyz/objkt/12345',
-  image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
-},{
-  title: 'Demo 2',
-  description: 'Filler Image\n\nDemo 2 is another demo artwork to test the exhibitions\n\nIt will be replaced with the artists works',
-  url: 'https://hicetnunc.xyz/objkt/23456',
-  image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1200&q=60'
-},{
-  title: 'Demo 3',
-  description: 'Filler Image\n\nDemo 3 is yet another demo artwork to test the exhibitions\n\nIt will also be replaced with the artists works',
-  url: 'https://hicetnunc.xyz/objkt/34568',
-  image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60'
-}]
-
 export const ExhibitionPageTemplate = ({
   title,
   image,
@@ -33,7 +16,7 @@ export const ExhibitionPageTemplate = ({
   <div className="content">
     <section className="exhibition-page">
       <h1 className="has-text-centered">{title}</h1>
-      { test_objkts.map((objkt, index) => (
+      { objkts.map((objkt, index) => (
         <div className="exhibition-page-objkt" key={index}>
           <div className="exhibition-page-objkt-left">
             <button className="exhibition-page-button" onClick={() => {
@@ -43,13 +26,13 @@ export const ExhibitionPageTemplate = ({
           </div>
           <div className="exhibition-page-objkt-right">
             <h2>{objkt.title}</h2>
-            <p>{objkt.description}</p>
+            <p>{objkt.desc}</p>
             <a target="_blank" rel="noreferrer" href={objkt.url} className="button is-primary">VIEW / COLLECT</a>
           </div>
         </div>
       ))}
       <Exhibition
-        objkts={test_objkts}
+        objkts={objkts}
       />
       {/* <Exhibition
         objkts={objkts}
@@ -100,7 +83,12 @@ export const exhibitionPageQuery = graphql`
       frontmatter {
         title
         description
-        objkts
+        objkts {
+          title
+          desc
+          image
+          objkt
+        }
         artist
         image {
           childImageSharp {
