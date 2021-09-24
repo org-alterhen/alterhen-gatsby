69fedcc4785228f67a66ea45cd09b35da14527e4
kylegrover <kyle@kylegrover.com>
2021-09-24 14:08:46 -0700

    fix diane exhib, add long bio, home/about styles

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -15,7 +15,7 @@ export const AboutPageTemplate = ({ title, content, contentComponent }) => {
         <div className="columns">
           <div className="column is-10 is-offset-1">
             <div className="section">
-              <h1 className="title is-size-3 has-text-weight-bold is-bold-light has-text-centered">
+              <h1 className="title is-size-3 has-text-weight-bold is-bold-light has-text-centered" style={{color: 'black'}}>
                 <Logo/>
               </h1>
               <PageContent className="content" content={content} />
