fc3a4b77aa1e83378387af40b5c4600379b31e38
kylegrover <kyle@kylegrover.com>
2021-09-26 12:16:52 -0700

    square logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -13,12 +13,12 @@ LogoText.propTypes = {
 export const LogoIcon = ({ className }) => (
   <span className={`logo-icon ${className}`}>
     <svg width="272" height="272" viewBox="0 0 272 272" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <rect y="6.10352e-05" width="46.6667" height="272" rx="6" fill="currentColor"/>
-    <rect x="29.335" y="39.1686" width="46.6667" height="306.01" rx="6" transform="rotate(-40.5456 29.335 39.1686)" fill="currentColor"/>
-    <rect x="272" width="46.6667" height="272" rx="6" transform="rotate(90 272 0)" fill="currentColor"/>
-    <rect x="272" y="106.665" width="46.6667" height="272" rx="6" transform="rotate(90 272 106.665)" fill="currentColor"/>
-    <rect x="272" y="225.333" width="46.6667" height="272" rx="6" transform="rotate(90 272 225.333)" fill="currentColor"/>
-    <rect x="225.333" width="46.6667" height="272" rx="6" fill="currentColor"/>
+    <rect y="6.10352e-05" width="46.6667" height="272" fill="currentColor"/>
+    <rect x="29.335" y="39.1686" width="46.6667" height="306.01" transform="rotate(-40.5456 29.335 39.1686)" fill="currentColor"/>
+    <rect x="272" width="46.6667" height="272" transform="rotate(90 272 0)" fill="currentColor"/>
+    <rect x="272" y="106.665" width="46.6667" height="272" transform="rotate(90 272 106.665)" fill="currentColor"/>
+    <rect x="272" y="225.333" width="46.6667" height="272" transform="rotate(90 272 225.333)" fill="currentColor"/>
+    <rect x="225.333" width="46.6667" height="272" fill="currentColor"/>
     </svg>
   </span>
 )
