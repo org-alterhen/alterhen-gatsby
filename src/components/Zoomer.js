eed7172ab6967bb68b7acb342921a37d3bde8487
kylegrover <kyle@kylegrover.com>
2021-09-29 20:32:08 -0700

    zoomier zoomies

--- src/components/Zoomer.js
+++ src/components/Zoomer.js
@@ -1,5 +1,29 @@
 import React from 'react'
 
+/* View in fullscreen */
+function openFullscreen() {
+  var elem = document.documentElement;
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+
+/* Close fullscreen */
+function closeFullscreen() {
+  var elem = document;
+  if (elem.exitFullscreen) {
+    elem.exitFullscreen();
+  } else if (elem.webkitExitFullscreen) { /* Safari */
+    elem.webkitExitFullscreen();
+  } else if (elem.msExitFullscreen) { /* IE11 */
+    elem.msExitFullscreen();
+  }
+}
+
 const Zoomer = ({ children }) => {
   // zoom state
   const [zoom, setZoom] = React.useState(false)
@@ -7,6 +31,13 @@ const Zoomer = ({ children }) => {
   return (
     <button className={`exhibition-page-button`+(zoom?' exhibition-zoomed':'')} onClick={() => {
         setZoom(!zoom)
+        if (zoom) {
+          console.log('closing')
+          closeFullscreen()
+        } else {
+          console.log('opening')
+          openFullscreen()
+        }
     }}>
         {children}
     </button>
